65c2fd1d7b5893b90a9f1f94ec4883c98409bc45
!  va1les <67857820+va1les@users.noreply.github.com>
2023-11-10 19:57:56 +0500

    Update bio_script.js

--- js/bio_script.js
+++ js/bio_script.js
@@ -15,11 +15,11 @@ function localStorageFunction() {
 const commandJson =
 {
   "auto-poster": {
-    text: `Auto Poster — полезный инструмент для автоматической отправки сообщений от вашего лица.<br><br><a class="messageA">https://discord.gg/SJK5uRqqEs</a> — официальный дискорд Auto Poster'а.<br><br><i class="fa fa-shopping-cart" aria-hidden="true"></i> 3 дня бесплатного использования — FREE3DAYS. <a class="small"></a>`,
+    text: `Auto Poster — полезный инструмент для автоматической отправки сообщений от вашего лица в дискорд.<br><br><a class="messageA">https://discord.gg/SJK5uRqqEs</a> — официальный дискорд Auto Poster'а.`,
     delay: 1000
   },
-  "discord-bots": {
-    text: `Экономика — Договорная;<br>Ваше ТЗ — Договорная;<br>Модерация — от 300₽;<br>Музыка — от 300₽;<br>Баннер — от 200₽;<br>Логирование — от 300₽;<br>Автороли — от 300₽;<br>Тикеты — от 300₽;<br>Верификация — от 200₽;<br><s>ChatGPT — от 200₽.</s>`,
+  "discord": {
+    text: `Создаю Discord Bot'ов любой сложности. Готов помочь с кодом или реализовать индивидуальные заказы. Обеспечиваю высококачественные и персонализированные решения для вашего сервера.`,
     delay: 2000
   },
   contact: {
@@ -27,13 +27,13 @@ const commandJson =
     delay: 1000
   },
   donate: {
-    text: `**Вы можете поддержать меня!**<br><br><a class="sber">Sber:</a> 2202206703020607<br><a class="qiwi">Qiwi:</a> <a class="link" href="https://qiwi.com/n/VAILES" target="_blank">VAILES</a><br><a class="qiwi">DonationAlerts: </a><a class="link" href="https://www.donationalerts.com/r/va1les" target="_blank">Кликабельно</a><br>`,
+    text: `**Вы можете поддержать меня!**<br><br><a class="sber">Sber:</a> 2202 2067 7298 0749<br><a class="qiwi">Qiwi:</a> <a class="link" href="https://qiwi.com/n/VAILES" target="_blank">VAILES</a><br><a class="qiwi">DonationAlerts: </a><a class="link" href="https://www.donationalerts.com/r/va1les" target="_blank">Кликабельно</a><br>`,
     delay: 1000
   }
 };
 
 let prefix = '/';
-let cmds = ['auto-poster', 'discord-bots', 'contact', 'donate'];
+let cmds = ['auto-poster', 'discord', 'contact', 'donate'];
 
 function botTyping(typing_time, answer_time) {
   let messageContainer = document.getElementById("messageContainer");
@@ -94,7 +94,7 @@ function sendMessage(content) {
   let messageContent = content === undefined ? messageInput.value.trim() : content;
   if (messageContent.length > 2000) {
     messageInput.value = "";
-    return botAnswer("Ваше сообщение не было отправлено.", 1000, 0, 0, 2000, "Система");
+    return botAnswer("Ваше сообщение не было отправлено.", 1000, 0, 0, 5000, "Система");
   } else if (messageContent !== "") {
     if (random <= 10) {
       botAnswer(`<i class="fa fa-music link"></i> <a class="link" href="https://va1les.ru/pitch" target="_blank">https://va1les.ru/pitch</a><br><i class="fas fa-clock link"></i> <a class="link" href="https://va1les.ru/timer" target="_blank">https://va1les.ru/timer</a>`, 0, 0, 0, 8000, "Система")
@@ -176,6 +176,12 @@ window.addEventListener("DOMContentLoaded", function () {
   let time = 3000
   let loaderContainer = document.querySelector(".loader-container");
 
+  const userInfo = document.querySelector('.user-info');
+
+  userInfo.addEventListener('click', function () {
+    showPopup()
+  });
+
   setTimeout(function () {
     loaderContainer.classList.add("fade-out");
   }, 1000);
@@ -190,20 +196,14 @@ window.addEventListener("DOMContentLoaded", function () {
   } else {
     time += 1500
     botAnswer(
-      `Привет! Меня зовут Паша, мне 16 лет. Я разработчик ботов для Discord. Готов помочь с созданием бота для вашего сервера.`,
+      `Добро пожаловать!`,
       1000,
       1000,
       0
     );
 
-    const userInfo = document.querySelector('.user-info');
-
-    userInfo.addEventListener('click', function () {
-      showPopup()
-    });
-
     botAnswer(
-      `**Команды чат-бота:** <br>
+      `Команды чат-бота:<br>
         ${cmds.map(cmd => `<a class="messageA" id="${cmd}">${prefix}${cmd}</a><br>`).join('')}`,
       1000,
       1000,
